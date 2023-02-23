e581f31d3ffba11336f63f8ad32b29d66000c67e
LunaMonn <pakoszborys@gmail.com>
2023-02-23 16:04:13 +0100

    2com

---
+++ cw1/1_2_8/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a,b,c;
+    float srednia;
+    scanf("%i",&a);
+    scanf("%i",&b);
+    scanf("%i",&c);
+    srednia=(a+b+c)/3;
+    printf("Srednia %lf",srednia);
+    return 0;
+}
